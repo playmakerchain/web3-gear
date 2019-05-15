b538ffe30351ad3fff22f1cc88e3513895cf2d7b
realchainlife <realchainlife@protonmail.com>
2019-05-15 16:51:21 +0300

    modify copyright listing

--- gear/powerplay/request.py
+++ gear/powerplay/request.py
@@ -1,3 +1,9 @@
+// Copyright (c) 2018 The VeChainThor developers
+// Copyright (c) 2019 The PlayMaker developers
+
+// Distributed under the GNU Lesser General Public License v3.0 software license, see the accompanying
+// file LICENSE or <https://www.gnu.org/licenses/lgpl-3.0.html>
+
 import lru
 import json
 import requests
