a1112ba339ce482cfec299768729f97c8442a809
realchainlife <realchainlife@protonmail.com>
2019-05-15 16:29:18 +0300

    modify copyright listing

--- gear/utils/compat.py
+++ gear/utils/compat.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2018 The VeChain developers
+# Copyright (c) 2018 The VeChainThor developers
 # Copyright (c) 2019 The PlayMaker developers
 
 # Distributed under the GNU Lesser General Public License v3.0 software license, see the accompanying
