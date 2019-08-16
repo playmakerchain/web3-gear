39e41549f74ed783331e9ce9e79dead7f9e1cb54
realchainlife <realchainlife@protonmail.com>
2019-08-16 21:29:41 +0300

    modify docker_entrypoint port descriptions

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -eo pipefail
 
 # if command starts with an option, prepend web3-gear
-if [ "${1:0:1}" = '-' ]; then
+if [ "${1:0:0}" = '-' ]; then
 	set -- web3-gear "$@"
 fi
 
@@ -20,25 +20,25 @@ else
   LISTEN_PORT="${LISTEN_PORT}"
 fi
 
-# Check if THOR_IP is not set
-if [[ -z "${THOR_IP}" ]]; then
-  echo "Env variable needed: eg. THOR_IP=127.0.0.1 or THOR_IP=www.example.com"
+# Check if POWERPLAY_IP is not set
+if [[ -z "${POWERPLAY_IP}" ]]; then
+  echo "Env variable needed: eg. POWERPLAY_IP=127.0.0.1 or POWERPLAY_IP=www.example.com"
   exit 1
 fi
 
-# check if THOR_PORT is not set
-if [[ -z "${THOR_PORT}" ]]; then
-  echo "Env variable needed: eg. THOR_PORT=2843"
+# check if POWERPLAY_PORT is not set
+if [[ -z "${POWERPLAY_PORT}" ]]; then
+  echo "Env variable needed: eg. POWERPLAY_PORT=2843"
   exit 1
 fi
 
-# check if THOR_PROTOCOL is not set
-if [[ -z "${THOR_PROTOCOL}" ]]; then
-  THOR_PROTOCOL='http'
+# check if POWERPLAY_PROTOCOL is not set
+if [[ -z "${POWERPLAY_PROTOCOL}" ]]; then
+  POWERPLAY_PROTOCOL='http'
 else
-  THOR_PROTOCOL="${THOR_PROTOCOL}"
+  POWERPLAY_PROTOCOL="${POWERPLAY_PROTOCOL}"
 fi
 
-echo "Using thor point: ${THOR_PROTOCOL}://${THOR_IP}:${THOR_PORT}"
+echo "Using POWERPLAY point: ${POWERPLAY_PROTOCOL}://${POWERPLAY_IP}:${POWERPLAY_PORT}"
 
-LC_ALL="C.UTF-8" LANG="C.UTF-8" web3-gear --host ${LISTEN_HOST} --port ${LISTEN_PORT} --endpoint "${THOR_PROTOCOL}://${THOR_IP}:${THOR_PORT}"
+LC_ALL="C.UTF-8" LANG="C.UTF-8" web3-gear --host ${LISTEN_HOST} --port ${LISTEN_PORT} --endpoint "${POWERPLAY_PROTOCOL}://${POWERPLAY_IP}:${POWERPLAY_PORT}"
